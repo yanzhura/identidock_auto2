62b9cf5d04956b33c6b5fa6b5ba3abb0cfa4c0c3
Mikhail Yanzhura <yanzhura@gmail.com>
2021-01-19 17:30:41 +0300

    Added jenkins.yml

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -2,21 +2,21 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
+import html
 
-app = Flask(__name__)
+app = Flask( __name__ )
 cache = redis.StrictRedis( host='redis', port=6379, db=0 )
 salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route( '/', methods=['GET', 'POST'] )
 def mainpage():
 
 	name = default_name
 	if request.method == 'POST':
-		name = request.form['name']
+		name = html.escape( request.form['name'], quote=True )
 	salted_name = salt + name
 	name_hash = hashlib.sha256( salted_name.encode() ).hexdigest()
-
 	header = '<html><head><title>Identidock</title></head><body>'
 	body = '''<form method="POST">
 		Hello <input type="text" name="name" value="{0}">
@@ -29,19 +29,19 @@ def mainpage():
 
 	return header + body + footer
 
-@app.route('/monster/<name>')
+@app.route( '/monster/<name>' )
 def get_identicon( name ):
-	
+
+	name = html.escape( name, quote=True )
 	image = cache.get( name )
 	if image is None:
-		print( "Cache miss (кэш отсутствует)", flush=True )
-	r = requests.get( 'http://dnmonster:8080/monster/' + name + '?size=80' )
-	image = r.content
-	cache.set( name, image )
+		print( "Cache miss (Промах кэша)", flush=True )
+		r = requests.get( 'http://dnmonster:8080/monster/' + name + '?size=80' )
+		image = r.content
+		cache.set( name, image )
 
 	return Response( image, mimetype='image/png' )
 
 if __name__ == '__main__':
 	app.run( debug=True, host='0.0.0.0' )
-
 
